660b1c2cf477128d5a7d1993f0d471a0ed69d999
WagnerCruz <wagnerfcruz@gmail.com>
2018-07-23 09:40:20 -0300

    update

--- app/src/main/java/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -1,10 +1,11 @@
-package com.whatsappandroid.cursoandroid.whatsapp;
+package com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.whatsappandroid.cursoandroid.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
